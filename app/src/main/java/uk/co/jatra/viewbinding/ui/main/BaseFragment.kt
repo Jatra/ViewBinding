336438bc4d97f51ad2622dae3bbb9ecb15c27054
Tim Russell <trussell@zipcar.co.uk>
2021-05-09 19:47:37 +0100

    Move ViewModel to base class generic

--- app/src/main/java/uk/co/jatra/viewbinding/ui/main/BaseFragment.kt
+++ app/src/main/java/uk/co/jatra/viewbinding/ui/main/BaseFragment.kt
@@ -5,16 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseFragment<VB: ViewBinding>: Fragment() {
+abstract class BaseFragment<VB : ViewBinding, VM : ViewModel> : Fragment() {
 
+    val viewModel by viewModels<MainViewModel>()
 
     private var _binding: VB? = null
 
     // This property is only valid between onCreateView and onDestroyView.
     protected val binding
-        get() = _binding?: throw IllegalStateException("Cannot access view in after view destroyed and before view creation")
+        get() = _binding
+            ?: throw IllegalStateException("Cannot access view in after view destroyed and before view creation")
 
     abstract val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> VB
 
--- app/src/main/java/uk/co/jatra/viewbinding/ui/main/MainFragment.kt
+++ app/src/main/java/uk/co/jatra/viewbinding/ui/main/MainFragment.kt
@@ -1,25 +1,19 @@
 package uk.co.jatra.viewbinding.ui.main
 
-import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import dagger.hilt.android.AndroidEntryPoint
 import uk.co.jatra.viewbinding.databinding.MainFragmentBinding
-import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainFragment : BaseFragment<MainFragmentBinding>() {
+class MainFragment : BaseFragment<MainFragmentBinding, MainViewModel>() {
 
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> MainFragmentBinding
         get() = MainFragmentBinding::inflate
 
-    val viewModel by viewModels<MainViewModel>()
-
     override fun setObservers() {
-        viewModel.liveData.observe(viewLifecycleOwner, Observer<Int> { update(it)} )
+        viewModel.liveData.observe(viewLifecycleOwner, Observer<Int> { update(it) })
     }
 
     override fun setListeners() {
